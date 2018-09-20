5f1790c33e66a8c985677a2489fba7faa70d403c
Tee <i@lado.me>
2018-09-20 10:34:17 +0800

    as a package

---
+++ __init__.py
@@ -0,0 +1,29 @@
+__all__ = [
+    'sms_service_AccountService',
+    'sms_service_AdgroupService',
+    'sms_service_BulkJobService',
+    'sms_service_CampaignService',
+    'sms_service_CreativeService',
+    'sms_service_DynamicCreativeService',
+    'sms_service_KeywordService',
+    'sms_service_KRService',
+    'sms_service_LiveReportService',
+    'sms_service_NewCreativeService',
+    'sms_service_ReportService',
+    'sms_service_SearchService',
+    'sms_service_ToolkitService',
+]
+
+from .sms_service_AccountService import *
+from .sms_service_AdgroupService import *
+from .sms_service_BulkJobService import *
+from .sms_service_CampaignService import *
+from .sms_service_CreativeService import *
+from .sms_service_DynamicCreativeService import *
+from .sms_service_KeywordService import *
+from .sms_service_KRService import *
+from .sms_service_LiveReportService import *
+from .sms_service_NewCreativeService import *
+from .sms_service_ReportService import *
+from .sms_service_SearchService import *
+from .sms_service_ToolkitService import *
